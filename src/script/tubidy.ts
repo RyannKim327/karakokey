62c7a6111a2b99839f2685fa8d91d9b23820b844
RyannKim327 <rksesgundo123@gmail.com>
2026-05-22 00:30:09 +0000

    Merge pull request #19 from RyannKim327/ryannkim327/development
    ryannkim327/development

--- src/script/tubidy.ts
+++ src/script/tubidy.ts
@@ -1,92 +1,28 @@
-import axios from "axios"
-import * as cheerio from "cheerio"
-
-const endpoint = "https://mp3.tubidy.cool"
+import { Tubidy } from "tubidy-scrape";
+import { SearchResults } from "tubidy-scrape/dist/interfaces";
 
 export async function search(song: string) {
-	let results: { id: string | undefined; link: string; title: string | undefined }[] = []
-	let pages = 1
-	while (results.length <= 0 && pages <= 3) {
-		const { data } = await axios.get(`${endpoint}/search.php`, {
-			headers: {
-				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-			},
-			params: {
-				q: `${song} karaoke`,
-				si: 7,
-				pn: 1
-			}
-		})
-		const $ = cheerio.load(data)
-		const html = $("div.media-body")
+	const tubidy = Tubidy()
+	const songs = await tubidy.search(`${song} karaoke`, 3)
+	let results: SearchResults[] = []
 
-		html.each((i, e) => {
-			const content = $(e).find("a")
-			const href = content.attr("href")
-			const title = content.attr("aria-label")
-
-			if (href) {
-				const link = href.startsWith("//") ? `https:${href}` : href.startsWith("/") ? `${endpoint}${href}` : href
-				const idMatch = href.match(/\/watch\/([^\/]+)/)
-				const id = idMatch ? idMatch[1] : undefined
-				if (id && !href.includes("act=process")) {
-					results.push({
-						id,
-						link,
-						title
-					})
-				}
-			}
-		})
-		pages++
+	for (let i = 0; i < songs.length; i++) {
+		const s = songs[i]
+		const r = await tubidy.review(s.id)
+		if (r.success && !r.error) {
+			results.push(s)
+		}
 	}
 
 	return results
 }
 
 export async function play(id: string) {
-	const { data } = await axios.get(`${endpoint}/watch.php`, {
-		headers: {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-		},
-		params: {
-			id,
-			p: "mp4",
-			t: "ssl",
-			act: "down",
-			lnk: 3
-		}
-	})
-	const $ = cheerio.load(data)
-	const items = $("li.list-group-item.big")
-	let play: string | undefined
-	let download: string | undefined
-	let isProcess: boolean = false
-
-	items.each((i, e) => {
-		const a = $(e).find("a")
-		const text = a.text().toLowerCase()
-		const href = a.attr("href")
-
-		if (href) {
-			isProcess = href?.includes("act=process")
-		}
-
-		if (text.includes("play")) {
-			play = href
-		} else if (text.includes("download")) {
-			download = href
-		}
-	})
-
-	if (isProcess) {
-		return {
-			error: "This video is in process, probably error from the hosts"
-		}
-	}
-
-	return {
-		url: play,
-		download
+	const tubidy = Tubidy()
+	const play = await tubidy.download(id, "video")
+	if (play.play) {
+		return play.play
+	} else {
+		return play.error ?? "Something went wrong"
 	}
 }
