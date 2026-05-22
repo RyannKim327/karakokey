2174b1178500f4b461047ca97b1a31646610a839
RyannKim327 <rksesgundo123@gmail.com>
2026-05-22 09:57:59 +0800

    05-22-26  9:57

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,4 +1,4 @@
-const HOST = "karakokey.leapcell.app"
-const WS = "karakokey.leapcell.app"
+const HOST = "karakokey.osc-fr1.scalingo.io"
+const WS = "karakokey.osc-fr1.scalingo.io"
 export const API_HOST = `https://${HOST}`;
 export const WS_HOST = `wss://${WS}`;
