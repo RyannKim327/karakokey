7ff8475a622525b01419d61a50d53aca54fd3ef7
RyannKim327 <rksesgundo123@gmail.com>
2026-05-25 09:04:02 +0800

    05-25-26  9:04 [feat: pwa]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,34 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from "@tailwindcss/vite"
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/karakokey/",
   plugins: [
     svelte(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      }, manifest: {
+        name: "Karakokey",
+        short_name: "Karakokey",
+        description: "A digitalized KTV Machine",
+        theme_color: "#121212",
+        icons: [{
+          src: "karakokey.png",
+          sizes: '192x192',
+          type: "image/png"
+        }, {
+          src: "karakokey.png",
+          sizes: '512x512',
+          type: "image/png"
+        }]
+      }
+    })
   ],
   resolve: {
     alias: {
