c0144a2a029d89db17627fee4d4d1e2e56f7c322
RyannKim327 <rksesgundo123@gmail.com>
2026-05-26 18:12:40 +0800

    05-26-26 18:12 [feat: facebook stream]

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,5 +1,5 @@
 const local = false
-const HOST = local ? "http://192.168.0.116:3000" : "https://karakokey.osc-fr1.scalingo.io"
-const WS = local ? "ws://192.168.0.116:8080" : "wss://karakokey.osc-fr1.scalingo.io"
+const HOST = local ? "http://192.168.0.101:3000" : "https://karakokey.osc-fr1.scalingo.io"
+const WS = local ? "ws://192.168.0.101:8080" : "wss://karakokey.osc-fr1.scalingo.io"
 export const API_HOST = HOST;
 export const WS_HOST = WS;
