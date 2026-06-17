c192bb14464e6748dd2b879c3618d116c850039c
RyannKim327 <rksesgundo123@gmail.com>
2026-06-17 21:55:56 +0800

    06-17-26 21:55

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,5 +1,5 @@
 const local = false
-const HOST = local ? `http://${window.location.hostname}:3000` : "https://karakokey.osc-fr1.scalingo.io"
+const HOST = local ? `http://${window.location.hostname}:3000` : "https://karaoke-ls7t.onrender.com"
 const WS = HOST.replace(/^http/, "ws")
 
 export const API_HOST = HOST;
